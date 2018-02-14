b42ad7247bc7c0fd956b0472d38b81d790c16c16
Nick Brown <n.brown.professional@gmail.com>
2018-02-14 14:05:00 -0700

    Adds questions text to html
    -adds the HTML text that has all 8 questions (content and buttons) to the document
    -adds 4 images to be used for backgrounds to the images folder
    -psuedocode for the JavaScript game logic

---
+++ assets/javascript/app.js
@@ -0,0 +1,22 @@
+// trivia game logic
+
+// 1. first screen is displayed with button to click to start game
+    // a. button is listening for click
+    // b. when clicked, the initial screen that is in a div is then display none and div with gameplay questions is displayed
+// 2. second screen is displayed
+    // a. timer is started at 150 seconds and is added to the div with "time remaining"
+    // b. if time runs out the the gameplay screen switches to display none and game over screen displays
+        // i. game over screen has loss displayed (optional) and totall wins and losses displayed
+        // ii. # of correct answers are displayed in the "Correct Answers" section
+        // iii. # of incorrect answers are displayed in the "Incorrect Answers" section
+        // iv. # of unanswered questions are displayed in "Unanswered" section
+        // (optional) v. reset button - when pressed resets the game
+    // c. each question (8 total) has 4 radio buttons 
+    // d. each question must have a radio button clicked before form will submit
+    // e. when submit button is pressed current screen is set to display none and game over screen is displayed
+        // i. game over screen has win displayed (optional) and total wins and losses
+        // ii. # of correct answers are displayed in the "Correct Answers" section
+        // iii. # of incorrect answers are displayed in the "Incorrect Answers" section
+        // iv. # of unanswered questions are displayed in "Unanswered" section
+        // (optional) v. reset button - when pressed resets the game
+     
